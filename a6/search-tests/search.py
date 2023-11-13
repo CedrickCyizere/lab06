804d25177abdfcae5eae3c9ca37474466470eeb4
cedrickcyizere <cedrickcyizere@gmail.com>
2023-11-12 22:34:14 -0800

    A6--Changed search.py modified hard coded path and added search

--- a6/search-tests/search.py
+++ a6/search-tests/search.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 from collections import defaultdict
+import sys
+import numpy as np
 
 def create_term_document_matrix(directory):
     # Initialize a defaultdict of type int
@@ -26,6 +28,30 @@ def create_term_document_matrix(directory):
 
     return df
 
-# Use the function
-df = create_term_document_matrix('/cedrickcyizere/a6')
-print(df)
+def cosine_similarity(A, B):
+    dot_product = np.dot(A, B)
+    magnitude = np.linalg.norm(A) * np.linalg.norm(B)
+    return dot_product / magnitude
+
+def main():
+    # Get the directory path from the command line arguments
+    directory = sys.argv[1]
+
+    # Create the term-document matrix
+    df = create_term_document_matrix(directory)
+
+    # Read the query vector from stdin
+    query_vector = np.array([float(x) for x in input().split()])
+
+    # Compute the cosine similarity of the query vector with all document vectors
+    similarities = {file: cosine_similarity(query_vector, df[file].values) for file in df.columns}
+
+    # Sort the documents by similarity
+    sorted_similarities = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
+
+    # Dump the similarity and document name pairs into stdout
+    for file, similarity in sorted_similarities:
+        print(f'{similarity} {file}')
+
+if __name__ == '__main__':
+    main()
